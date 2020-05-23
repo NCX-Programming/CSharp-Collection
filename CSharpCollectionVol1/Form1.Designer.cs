39c3341d73a1f96772f3341076ca2e58cbcdc65f
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2020-05-23 10:13:06 -0400

    nothing important changed

--- CSharpCollectionVol1/Form1.Designer.cs
+++ CSharpCollectionVol1/Form1.Designer.cs
@@ -67,7 +67,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(28, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "v0.3";
+            this.label2.Text = "v1.0";
             // 
             // label3
             // 
